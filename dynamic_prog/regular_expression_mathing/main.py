1a71f50ec016e6eb14ae635cd5dc27bedfc80455
ArtDub <rusartdub@gmail.com>
2019-07-22 16:05:10 +0700

    regular_exp

---
+++ dynamic_prog/regular_expression_mathing/main.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        mem = {}
+
+        def dp(i, j):
+
+            if (i, j) not in mem:
+                if len(p) == j:
+                    ans = len(s) == i
+                else:
+                    fst_match = i < len(s) and p[j] in {s[i], '.'}
+                    if j + 1 < len(p) and p[j + 1] == '*':
+                        ans = dp(i, j + 2) or (fst_match and dp(i + 1, j))
+                    else:
+                        ans = fst_match and dp(i + 1, j + 1)
+                mem[i, j] = ans
+            return mem[i, j]
+
+        return dp(0, 0)
+
+
+t, p = input(), input()
+
+sol = Solution()
+
+print(sol.isMatch(t, p))
