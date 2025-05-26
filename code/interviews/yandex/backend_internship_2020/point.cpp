5e8c93170d40470e5247416b1c7aa71bb9c0ef4d
Artyom Dubinin <rusartdub@gmail.com>
2025-05-26 12:17:27 +0300

    Update point.cpp

--- code/interviews/yandex/backend_internship_2020/point.cpp
+++ code/interviews/yandex/backend_internship_2020/point.cpp
@@ -1,32 +1,39 @@
-//Нужно узнать можно ли провести вертикальную прямую, чтобы все точки были симметричны
+// Нужно узнать можно ли провести вертикальную прямую, чтобы все точки были симметричны
+// https://www.lintcode.com/problem/908/
 
-bool f(vector<pnt> &points){
-    
-    int n = points.size();
-    
-    int mid;
-    for(int i = 0; i < n; ++i){
-        mid += (points[i].x * 2);
-    }
-    if(mid % n != 0) return false;
-    mid /= n;
-    
-    
-    map<pair<int, int>, int> mp;
-    for(int i = 0; i < n; ++i){
-        int y = points[i].y;
-        int x = points[i].x * 2;
-        
-        x -= mid;
-        mp[{y, abs(x)}]++;
-    }
-    
-    for(auto &[k, v] : mp){
-        if(v % 2 != 0){
-            return false;
+class Solution {
+public:
+    /**
+     * @param points: n points on a 2D plane
+     * @return: if there is such a line parallel to y-axis that reflect the given points
+     */
+    bool isReflected(vector<vector<int>> &points) {
+        if (points.size() < 1) {
+            return true;
+        } 
+
+        int min_x = INT_MAX;
+        int max_x = INT_MIN;
+
+        map<pair<int, int>, int> mp;
+        for (int i = 0; i < points.size(); i++) {
+            int x = points[i][0];
+            int y = points[i][1];
+            min_x = min(min_x, x);
+            max_x = max(max_x, x);
+            mp[{x, y}]++;
         }
+        int candidate = max_x + min_x;
+        
+        for (auto& [k, v] : mp) {
+            int reflected_x = candidate - k.first;
+            int y = k.second;
+
+            if (mp[{reflected_x, y}] != v) {
+                return false;
+            }
+        }
+
+        return true;
     }
-    
-    
-    return true;
-}
+};
